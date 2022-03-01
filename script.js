b41bf9c0ab3c82535115b1407b180684f36400bc
Neil <88711171+21Neil@users.noreply.github.com>
2022-03-01 14:37:46 +0800

    add simple screen

--- script.js
+++ script.js
@@ -29,31 +29,54 @@ function playRound(playerSelection, computerSelection) {
   ) {
     return `You Loss! ${computerSelection} beats ${playerSelection
       .toLowerCase()
-      .replace(playerSelection[0], playerSelection[0].toUpperCase())}`;
+      .replace(
+        playerSelection[0].toLowerCase(),
+        playerSelection[0].toUpperCase()
+      )}`;
   } else {
     return `You Win! ${playerSelection
       .toLowerCase()
       .replace(
-        playerSelection[0],
+        playerSelection[0].toLowerCase(),
         playerSelection[0].toUpperCase()
       )} beats ${computerSelection}`;
   }
 }
 
-function game() {
-  let counter = 0;
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Rock, Paper, Scissors");
-    console.log(playerSelection)
-    const computerSelection = computerPlay();
-    console.log(computerSelection)
-    const result = playRound(playerSelection, computerSelection)
-    console.log(result);
-    if (result.includes('Win') === true){
-        counter += 1;
-    }
-    console.log(`You win ${counter} time`)
+const allBtn = document.querySelectorAll("button");
+const divResult = document.querySelector(".result");
+let playerPoint = 0;
+let computerPoint = 0;
+
+allBtn.forEach((btn) => {
+  btn.addEventListener("click", game);
+});
+
+function game(e) {
+  const playerSelection = e.path[0].value;
+  console.log(playerSelection);
+  const computerSelection = computerPlay();
+  console.log(computerSelection);
+  const result = playRound(playerSelection, computerSelection);
+  console.log(result);
+  divResult.textContent = result;
+  counterPoint(result);
+  console.log(`You: ${playerPoint}\nComputer: ${computerPoint}`);
+  judge();
+}
+
+function counterPoint(result) {
+  if (result.includes("Win") === true) {
+    playerPoint += 1;
+  } else if (result.includes("Loss") === true) {
+    computerPoint += 1;
   }
 }
 
-game()
+function judge() {
+  if (playerPoint === 5) {
+    console.log('game over')
+  } else if (computerPoint === 5){
+    console.log('game over')
+  }
+}
